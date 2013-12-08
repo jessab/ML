5cb8e4bfe5d081f3bca5e5408b5c51e0c7631646
jessab <jessa.bekker@gmail.com>
2013-12-08 18:42:11 +0100

    vel

--- Solution/Code/features/Velocity.py
+++ Solution/Code/features/Velocity.py
@@ -3,13 +3,12 @@ Created on 5-dec.-2013
 
 @author: Koen
 '''
-import dataTransform.accproc as ap
-import matplotlib.pyplot as ppl
+import dataTransform.accproc as ac
 import pandas as pd
 
 def get1DVel(time,accSet,peaks):
     velocity = [(time[0],0)]
-    accSet = ap.savitzky_golay(accSet, 11, 4)
+    accSet = ac.savitzky_golay(accSet, 11, 4)
     window_size = 32
     nbSamples = len(time)/window_size
     for i in range(nbSamples-1):
@@ -23,29 +22,51 @@ def get1DVel(time,accSet,peaks):
 #         if (abs(da) < 0.010):
 #             da = 0
         veli = (velocity[-1][1] + dt*da/2)
-        if (peaks and peaks[0] <= time[k+window_size] ):
+        if (len(peaks)>0 and peaks[0] <= time[k+window_size] ):
             veli = 0
-            peaks.pop(0)
+            peaks = peaks[:-1]
         velocity.append((time[k+window_size],veli))
     velocity = zip(*velocity)
     velocity = pd.Series(velocity[1],velocity[0])
-    print (velocity)
+#     print (velocity)
     return velocity
 
 def getVelocity(data):
     time = data.index
     
-    velocity = []
+    velocity = dict()
     for v in ["Ax","Ay","Az"] :
-        peaks = ac.detectPeaksGCDC(filtered, v)
-        peaks = zip(*peaks)
-#         ppl.plot(peaks[0],peaks[1])
-        peaks = [x for x in peaks[0]]
-    
-        vel = get1DVel(time, data[v], peaks);
-        velocity.append(vel)
+        peaks = np.transpose(ac.detectPeaksGCDC(data, v))[0]
+        velocity["V"+v[1:]] = get1DVel(time, data[v], peaks);
         
-    return pd.concat(velocity, axis=1)
+    return pd.DataFrame.from_dict(velocity)
+
+import numpy as np
+from tools.Tools import getFun, getCoVars
+
+def getMeans(data):
+    return getFun(data, np.mean, 'mean')
+
+def getMins(data):
+    return getFun(data, np.min, 'min')
+    
+def getMaxs(data):
+    return getFun(data, np.max, 'max')
+
+def getMedians(data):
+    return getFun(data, np.median, 'median')
+
+
+
+def getVelocityFeatures(data):
+    data = getVelocity(data)
+    features = getMeans(data)
+    features.update(getMins(data))
+    features.update(getMaxs(data))
+    features.update(getMedians(data))
+    features.update(getCoVars(data,'velocity'))
+    
+    return features
     
 if __name__ == '__main__':
     import dataTransform.accproc as ac
@@ -56,6 +77,9 @@ if __name__ == '__main__':
     
     velocity = getVelocity(filtered)
     print velocity
-    velocity.plot()
+    features = getVelocityFeatures(filtered)
+    print(features)
+    
+#     velocity.plot()
 #     ppl.plot(t,filtered.Ax)
-    ppl.show()
+#     ppl.show()
